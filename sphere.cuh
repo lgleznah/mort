3b8ab8ac03d07ce76f7615f457a5ca3f737fc7a7
lgleznah <luisglez02@gmail.com>
2023-10-07 21:24:26 +0200

    Implemented optimisation that does not go faster

--- sphere.cuh
+++ sphere.cuh
@@ -6,8 +6,9 @@
 #include "hittable.cuh"
 #include "materials.cuh"
 
-#define SOME_THREAD_ONLY(whatevs) {if ((threadIdx.x < 100) && (threadIdx.y < 100) && (blockIdx.x < 100) && (blockIdx.y < 100)) {whatevs;}}
+#include "data_union.cuh"
 
+#define SOME_THREAD_ONLY(whatevs) {if ((threadIdx.x < 100) && (threadIdx.y < 100) && (blockIdx.x < 100) && (blockIdx.y < 100)) {whatevs;}}
 
 class sphere : public hittable {
 
@@ -18,15 +19,13 @@ class sphere : public hittable {
 		__host__ __device__ 
 		sphere(point3 cen, float r, material* m) : center1(cen), radius(r), mat(m), moves(false) {
 			auto rvec = vec3(radius, radius, radius);
-			bbox = aabb(center1 - rvec, center1 + rvec);
 		}
-		__host__ __device__ sphere(point3 cen1, point3 cen2, float r, material* m) : center1(cen1), radius(r), mat(m) {
+
+		__host__ __device__ 
+		sphere(point3 cen1, point3 cen2, float r, material* m) : center1(cen1), radius(r), mat(m) {
 			moves = true;
 			center_vec = cen2 - cen1;
 			auto rvec = vec3(radius, radius, radius);
-			aabb box1(cen1 - rvec, cen2 + rvec);
-			aabb box2(cen2 - rvec, cen2 + rvec);
-			bbox = aabb(box1, box2);
 		}
 
 		point3 center1;
@@ -35,12 +34,11 @@ class sphere : public hittable {
 		bool moves;
 		vec3 center_vec;
 
-		aabb bbox;
-
 		material* mat;
 		material* gpu_mat;
 
-		__device__ bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const override {
+		__device__ 
+		bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const override {
 			const vec3 oc = r.origin() - center(r.time());
 			const auto a = r.direction().length_squared();
 			const auto half_b = dot(oc, r.direction());
@@ -75,37 +73,26 @@ class sphere : public hittable {
 			return center1 + time * center_vec;
 		}
 
-		int toDevice(hittable** list, int idx) override;
+		int toDevice(hittable** list, int idx, int& mat_idx, data_union* ptr, mat_union* mat_ptr) override;
 
 		void freeFromDevice() const override {
 			HANDLE_ERROR(cudaFree(gpu_obj));
 		}
 
-		__device__ __host__ aabb bounding_box() const override { return bbox; }
 };
 
-__global__ void sphereToDevice(sphere cpu_sphere, hittable** list, int idx, hittable** ptr) {
-	sphere* gpu_sphere = new sphere();
-	gpu_sphere->center1 = cpu_sphere.center1;
-	gpu_sphere->radius = cpu_sphere.radius;
-	gpu_sphere->moves = cpu_sphere.moves;
-	gpu_sphere->center_vec = cpu_sphere.center_vec;
-	gpu_sphere->gpu_mat = cpu_sphere.gpu_mat;
-	gpu_sphere->gpu_obj = gpu_sphere;
-	gpu_sphere->bbox = cpu_sphere.bbox;
-	list[idx] = gpu_sphere;
-	*ptr = gpu_sphere;
-}
+__global__ void sphereToDevice(sphere cpu_sphere, int idx, data_union* arr_ptr);
 
-int sphere::toDevice(hittable** list, int idx) {
+int sphere::toDevice(hittable** list, int idx, int& mat_idx, data_union* ptr, mat_union* mat_ptr) {
 	// Move material to GPU (if it wasn't already moved)
 	if (mat->gpu_mat == nullptr) {
-		mat->toDevice();
+		mat->toDevice(mat_idx, mat_ptr);
 		gpu_mat = mat->gpu_mat;
+		mat_idx += 1;
 	}
 	hittable** gpu_gpu_sph_ptr;
 	HANDLE_ERROR(cudaMalloc((void**)&gpu_gpu_sph_ptr, sizeof(hittable*)));
-	sphereToDevice<<<1, 1 >>>(*this, list, idx, gpu_gpu_sph_ptr);
+	sphereToDevice<<<1, 1 >>>(*this, idx, ptr);
 	HANDLE_ERROR(cudaMemcpy(&gpu_obj, gpu_gpu_sph_ptr, sizeof(hittable*), cudaMemcpyDeviceToHost));
 	HANDLE_ERROR(cudaFree(gpu_gpu_sph_ptr));
 	return idx + 1;
