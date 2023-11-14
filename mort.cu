0ea0cff0588eaa6a5fe3a8aae7a3979b7b42fc98
lgleznah <luisglez02@gmail.com>
2023-11-14 17:55:48 +0100

    Removed old comments

--- mort.cu
+++ mort.cu
@@ -325,7 +325,7 @@ int main(void) {
 	Camera cam;
 	hittable_list data;
 
-	int scene_idx = 5;
+	int scene_idx = 1;
 
 	switch(scene_idx) {
 		case 1:
@@ -387,49 +387,3 @@ int main(void) {
 
 	bitmap.anim_and_exit((void (*)(uchar4*, void*, int))update, (void (*)(void*))anim_exit);
 }
-/*
-#include <stdio.h>
-#include <stdint.h>
-
-typedef uint8_t mt;  // use an integer type
-
-__global__ void kernel(cudaTextureObject_t tex)
-{
-	int x = threadIdx.x;
-	int y = threadIdx.y;
-	mt val = tex2D<mt>(tex, x, y);
-	printf("%d, ", val);
-}
-
-int main(int argc, char** argv)
-{
-	cudaDeviceProp prop;
-	cudaGetDeviceProperties(&prop, 0);
-	printf("texturePitchAlignment: %lu\n", prop.texturePitchAlignment);
-	cudaTextureObject_t tex;
-	const int num_rows = 4;
-	const int num_cols = prop.texturePitchAlignment * 1; // should be able to use a different multiplier here
-	const int ts = num_cols * num_rows;
-	const int ds = ts * sizeof(mt);
-	mt* dataIn = (mt*) malloc(ds * sizeof(mt));
-	for (int i = 0; i < ts; i++) dataIn[i] = i;
-	mt* dataDev = 0;
-	cudaMalloc((void**)&dataDev, ds);
-	cudaMemcpy(dataDev, dataIn, ds, cudaMemcpyHostToDevice);
-	struct cudaResourceDesc resDesc;
-	memset(&resDesc, 0, sizeof(resDesc));
-	resDesc.resType = cudaResourceTypePitch2D;
-	resDesc.res.pitch2D.devPtr = dataDev;
-	resDesc.res.pitch2D.width = num_cols;
-	resDesc.res.pitch2D.height = num_rows;
-	resDesc.res.pitch2D.desc = cudaCreateChannelDesc<mt>();
-	resDesc.res.pitch2D.pitchInBytes = num_cols * sizeof(mt);
-	struct cudaTextureDesc texDesc;
-	memset(&texDesc, 0, sizeof(texDesc));
-	cudaCreateTextureObject(&tex, &resDesc, &texDesc, NULL);
-	dim3 threads(4, 4);
-	kernel << <1, threads >> > (tex);
-	cudaDeviceSynchronize();
-	printf("\n");
-	return 0;
-}*/
